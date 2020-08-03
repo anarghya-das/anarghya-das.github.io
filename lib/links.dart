5ec0ab853758212fa4344d32bc42f78b538d88ab
anarghya@buffalo.edu <anarghya@buffalo.edu>
2020-08-03 02:43:48 -0400

    fixed broken resume link

--- lib/links.dart
+++ lib/links.dart
@@ -4,4 +4,4 @@ const String GITHUB = "https://github.com/anarghya-das";
 const String LINKEDIN = "https://linkedin.com/in/anarghya-das";
 const String DEVPOST = "https://devpost.com/anarghya-das";
 const String EMAIL = "mailto:anarghya@buffalo.edu";
-const String RESUME = "/assets/files/anarghya_resume.pdf";
+const String RESUME = "/assets/assets/files/anarghya_resume.pdf";
