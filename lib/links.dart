5790de97e282a997cb31b9e5fc4d8fa8fb1984b6
anarghya@buffalo.edu <anarghya@buffalo.edu>
2021-03-06 16:42:24 -0500

    updated resume

--- lib/links.dart
+++ lib/links.dart
@@ -4,5 +4,5 @@ const String GITHUB = "https://github.com/anarghya-das";
 const String LINKEDIN = "https://linkedin.com/in/anarghya-das";
 const String DEVPOST = "https://devpost.com/anarghya-das";
 const String EMAIL = "mailto:anarghya@buffalo.edu";
-const String RESUME = "/assets/assets/files/anarghya_resume_tech.pdf";
+const String RESUME = "/assets/assets/files/anarghya_resume.pdf";
 const String MATRIX = "https://matrix.to/#/@anarghya:matrix.org";
